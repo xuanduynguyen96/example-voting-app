aca2b4cae60b2015276e38e971a77ac744ec97a4
xduyng <xuanduynguyen96@gmail.com>
2023-07-04 16:47:20 +0700

    update

--- vote/app.py
+++ vote/app.py
@@ -47,5 +47,5 @@ def hello():
     return resp
 
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
